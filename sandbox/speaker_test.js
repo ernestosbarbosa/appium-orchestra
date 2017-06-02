af84a68325e9c4dfb2b1ae989bc41e166e23ba60
Jonathan Lipps <jlipps@gmail.com>
2017-06-02 15:54:55 -0700

    add speaker code

---
+++ sandbox/speaker_test.js
@@ -0,0 +1,17 @@
+const { asyncify } = require('asyncbox');
+const Speaker = require('../lib/speaker');
+
+async function run () {
+  let s = new Speaker();
+  let lyrics = [
+    [4, "I used to write the code"],
+    [4, "that made the world go round"],
+    [4, "but now it gets on just fine"],
+    [4, "without anyone"],
+  ];
+  await s.speakLyrics(lyrics, 100);
+}
+
+if (require.main === module) {
+  asyncify(run);
+}
